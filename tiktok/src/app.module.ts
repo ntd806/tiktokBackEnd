39ef9bd3fb9aabef758de122e958253abe8695dd
ntd806 <ntd806@gmail.com>
2022-07-01 16:29:54 +0800

    Update base consist of connection mongoDB

--- tiktok/src/app.module.ts
+++ tiktok/src/app.module.ts
@@ -1,8 +1,15 @@
-import { Module } from '@nestjs/common';
-
+import { Module, RequestMethod, MiddlewareConsumer } from '@nestjs/common';
+import { AppController } from './app/controllers/app.controller';
+import { ConfigModule } from './config/config.module';
+import { LoggerModule } from './logger/logger.module';
+import { DatabaseModule } from './database/database.module';
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    DatabaseModule.forRoot(),
+    ConfigModule,
+    LoggerModule
+  ],
+  controllers: [AppController]
 })
-export class AppModule {}
+export class AppModule {
+}
--- tiktok/src/main.ts
+++ tiktok/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from './config/config.service';
+import { createDocument } from './swagger/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+import { HttpExceptionFilter } from './app/core/filter/exception.filter';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+dotenv.config();
+import 'reflect-metadata';
+
+(async () => {
+  const app = await NestFactory.create(AppModule, {cors: true});
+  // app.useGlobalFilters(new HttpExceptionFilter());
+  const configService = app.get(ConfigService);
+  SwaggerModule.setup('api/v1', app, createDocument(app));
+  await app.listen(configService.get().port);
+  console.info('SERVER IS RUNNING ON PORT', configService.get().port);
+})();
