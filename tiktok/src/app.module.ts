224378e3a72236796edc7bb17c65a206c2acdd54
ntd806 <ntd806@gmail.com>
2022-07-08 10:54:39 +0800

    Update config redis

--- tiktok/src/app.module.ts
+++ tiktok/src/app.module.ts
@@ -3,9 +3,8 @@ import { AppController } from './app/controllers/app.controller';
 import { ConfigModule } from './config/config.module';
 import { LoggerModule } from './logger/logger.module';
 import { DatabaseModule } from './database/database.module';
-import { UserController } from './app/controllers/user.controller';
 @Module({
     imports: [DatabaseModule.forRoot(), ConfigModule, LoggerModule],
-    controllers: [AppController, UserController]
+    controllers: [AppController]
 })
 export class AppModule {}
