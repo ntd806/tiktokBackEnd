d371764b6c4190e6791d869510f37f573d3ca931
ntd806 <ntd806@gmail.com>
2022-07-30 10:38:22 +0800

    Create new api social network for verify

---
+++ src/components/auth/dto/social.dto.ts
@@ -0,0 +1,52 @@
+import { IsString, IsNotEmpty, isNumber } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class SocialDto {
+    @ApiProperty({
+        description: 'Phone number of user'
+    })
+    @IsNotEmpty()
+    @IsNotEmpty()
+    @IsString()
+    email: string;
+
+    @ApiProperty({
+    description: 'MAC address'
+    })
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    mac: string;
+
+    @ApiProperty({
+    description: 'token of Google or Facebook'
+    })
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    token: string;
+
+    @ApiProperty({
+    description: 'Id of Google or Facebook'
+    })
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    id: string;
+
+    @ApiProperty({
+    description: 'fullname of user'
+    })
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    fullname: string;
+
+    @ApiProperty({
+    description: 'Is Google or not Google'
+    })
+    @ApiProperty()
+    @IsNotEmpty()
+    @isNumber()
+    isGoogle: number;
+}
