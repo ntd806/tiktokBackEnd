e6dd1bbfdac275e4ad021349c890097b7a5e09f1
ntd806 <ntd806@gmail.com>
2022-08-09 10:20:04 +0800

    log error

--- src/components/auth/auth.service.ts
+++ src/components/auth/auth.service.ts
@@ -105,6 +105,7 @@ export class AuthService {
                 message: 'the another device'
             };
         } catch (error) {
+            console.log(error);
             throw new NotFoundException(error);
         }
     }
