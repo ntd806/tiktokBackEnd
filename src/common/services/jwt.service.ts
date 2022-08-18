a89db0e869a4400345913c2f1a9787b16b7121fa
navidev <navidev.contact@gmail.com>
2022-08-18 22:35:59 +0700

    fix: no return access, find is not a function

--- src/common/services/jwt.service.ts
+++ src/common/services/jwt.service.ts
@@ -8,14 +8,31 @@ export class JwtService {
         private readonly jwt: NestJSJwtService,
     ) {}
 
-    async signToken(phone: string, fullname: string): Promise<{ access_token: string }> {
+    signToken(phone: string, fullname: string): { access_token: string } {
         const payload: JwtPayload = {
             sub: phone,
             fullname,
         };
         const secret = process.env.SECRET;
 
-        const token = await this.jwt.signAsync(payload, {
+        const token = this.jwt.sign(payload, {
+            expiresIn: '365d',
+            secret: secret
+        });
+
+        return {
+            access_token: token
+        };
+    }
+
+    async signAsyncToken(phone: string, fullname: string): Promise<{ access_token: string }> {
+        const payload: JwtPayload = {
+            sub: phone,
+            fullname,
+        };
+        const secret = process.env.SECRET;
+
+        const token = await this.jwt.sign(payload, {
             expiresIn: '365d',
             secret: secret
         });
