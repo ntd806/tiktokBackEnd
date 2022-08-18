b8d05418e4d9e8268c3d22867df618d3bbc0525e
navidev <navidev.contact@gmail.com>
2022-08-18 17:15:14 +0700

    fix: regex check phone

--- src/components/redis/verify.opt.dto.ts
+++ src/components/redis/verify.opt.dto.ts
@@ -4,10 +4,10 @@ import { ApiProperty } from '@nestjs/swagger';
 export class VerifyOtpDto {
     @ApiProperty()
     @IsNotEmpty()
-    @Matches(/(^\+d{1,4})\d{9,10}$/g, {
+    @Matches(/\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$/g, {
         message: 'Invalid phone number'
     })
-    phoneNumber: string;
+    phone: string;
 
     @ApiProperty()
     @Length(6, 6)
