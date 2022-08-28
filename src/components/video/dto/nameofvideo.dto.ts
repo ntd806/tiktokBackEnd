ead915bba614b989045a9517190f93152510bcc4
ntd806 <ntd806@gmail.com>
2022-08-28 15:28:40 +0800

    Remove getdetailvideo/api.

--- src/components/video/dto/nameofvideo.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsNotEmpty } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class NameVideoDto {
-    @IsNotEmpty()
-    @ApiProperty()
-    name: string;
-}
